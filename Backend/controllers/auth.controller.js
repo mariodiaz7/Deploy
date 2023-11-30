c364a9654933655ac97d25f79756d05108f56933
gabriela <00010122@uca.edu.sv>
2023-11-30 00:28:56 -0600

    SUbiendo el Login del usuario

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -29,6 +29,29 @@ controller.register = async (req, res, next) => {
   }
 }
 
+controller.login = async (req, res, next) => {
+    try {
+        //Obtener la info
+    const { username, email, password } = req.body;
+    //Verifircar existencia de correo y usuario
+    const user = await User.findOne({        $or: [{ username: identifier }, { email: identifier }] });
+      // Si no existe, retornar 404
+    if (!user) {
+        return res.status(404).json({ error: "User not found" });
+      }
+      //Si existe, verificar password
+      //Si password no coincide, retornar 401
+      if (!user.comparePassword(password)) {
+        return res.status(401).json({ error: "Incorrect Password" });
+      }
 
+      return res.status(200).json({message:"wiiiiiii"});
+
+      }catch(error){
+        console.error(error);
+        return res.status(500).json({error: "Internal Server Error"})
+    }
+}
+      
 
 module.exports = controller;
--- Backend/routes/auth.router.js
+++ Backend/routes/auth.router.js
@@ -11,5 +11,7 @@ router.post("/register",
   authController.register
 );
 
+router.post("/login", 
+authController.login);
 
 module.exports = router;
