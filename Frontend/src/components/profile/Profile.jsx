7c4c122c0646aa9a7518ed9ba7a305ebce2027bf
Maio Diaz <00020322@uca.edu.sv>
2023-12-01 22:29:48 -0600

    mostrar info perfil

--- Frontend/src/components/profile/Profile.jsx
+++ Frontend/src/components/profile/Profile.jsx
@@ -1,6 +1,18 @@
+import React from 'react';
 import ProfilePhoto from "../../img/3682281.png";
+import profileFetch from '../../services/profileFetch';
 
-function Profile() {
+const Profile = () => {
+  const { userData } = profileFetch();
+
+  if (!userData) {
+    return (
+      <div className="flex items-center justify-center h-500">
+        <div className="text-4xl font-bold text-gray-800">No has iniciado sesion...</div>
+      </div>
+    );
+  }
+
   return (
     <div className="flex flex-col justify-center items-center h-500">
       <div className="grid md:grid-cols-2 gap-20 px-5 md:px-20">
@@ -9,14 +21,12 @@ function Profile() {
         </div>
 
         <div className="flex flex-col justify-center text-center md:text-left">
-          <p className="pb-3 md:pb-5 text-xl md:text-3xl font-bold text-[#2F4D55]">Nombre: Carlitos</p>
-          <p className="pb-3 md:pb-5 text-xl md:text-3xl font-bold text-[#2F4D55]">Apellido: Canjura</p>
-          <p className="pb-3 md:pb-5 text-xl md:text-3xl font-bold text-[#2F4D55]">Email: papu777@uca.edu.sv</p>
-          <p className="pb-3 md:pb-5 text-xl md:text-3xl font-bold text-[#2F4D55]">Contraseña: momosalvmamu</p>
+          <p className="pb-3 md:pb-5 text-xl md:text-3xl font-bold text-[#2F4D55]">Username: {userData.username}</p>
+          <p className="pb-3 md:pb-5 text-xl md:text-3xl font-bold text-[#2F4D55]">Email: {userData.email}</p>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Profile;
