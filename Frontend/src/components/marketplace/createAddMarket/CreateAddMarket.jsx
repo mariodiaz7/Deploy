77460cbbdc0123511364132a5b2e6b74c2126d9a
Maio Diaz <00020322@uca.edu.sv>
2023-12-02 01:52:04 -0600

    product post

--- Frontend/src/components/marketplace/createAddMarket/CreateAddMarket.jsx
+++ Frontend/src/components/marketplace/createAddMarket/CreateAddMarket.jsx
@@ -1,22 +1,72 @@
+import React, { useState, useContext } from 'react';
+import createProduct from '../../../services/createProduct';
+import UserContext from '../../../context/Usercontext';
+
 function CreateAddMarket() {
+  const [productData, setProductData] = useState({
+    title: '',
+    description: '',
+    image: '',
+    productState: '',
+    price: 0,
+    contact: '',
+  });
+
+  const { jwt } = useContext(UserContext);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await createProduct(productData, jwt);
+      console.log('Producto creado correctamente'); // Muestra mensaje en la consola
+      // Puedes realizar acciones adicionales después de crear el producto, si es necesario
+    } catch (error) {
+      console.error('Error al crear el producto:', error.message);
+      // Puedes mostrar el mensaje de error al usuario, por ejemplo:
+      alert(`Error al crear el producto: ${error.message}`);
+    }
+  };
+
+  const handleChange = (e) => {
+    setProductData({
+      ...productData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleImageChange = (e) => {
+    // Aquí puedes manejar la carga de imágenes según tus necesidades
+    const files = e.target.files;
+    setProductData({
+      ...productData,
+      image: files,
+    });
+  };
+
   return (
     <div className="px-4 lg:px-10">
       <h3 className="text-2xl lg:text-4xl font-medium text-[#1F3A44] mb-6 lg:mb-10">
         Crea tu anuncio
       </h3>
 
-      <form className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+      <form className="grid grid-cols-1 lg:grid-cols-2 gap-6" onSubmit={handleSubmit}>
         <div className="flex flex-col">
-          <input
-            type="file"
-            accept="image/*"
-            multiple
+        <input
+            type="text"
+            name="image"
+            value={productData.image}
+            onChange={handleChange}
             className="m-3 lg:m-5 shadow appearance-none border border-gray-300 rounded h-full py-3 lg:py-4 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            placeholder="Agregar imágenes"
+            placeholder="URL de la imagen"
           />
+
 
           <input
             type="text"
+            name="description"
+            value={productData.description}
+            onChange={handleChange}
             className="m-3 lg:m-5 shadow appearance-none border border-gray-300 rounded h-full py-3 lg:py-4 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             placeholder="Descripción"
           />
@@ -25,38 +75,50 @@ function CreateAddMarket() {
         <div className="flex flex-col">
           <input
             type="text"
+            name="title"
+            value={productData.title}
+            onChange={handleChange}
             className="m-3 lg:m-5 shadow appearance-none border border-gray-300 rounded h-full py-3 lg:py-4 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             placeholder="Título"
           />
 
           <input
             type="number"
+            name="price"
+            value={productData.price}
+            onChange={handleChange}
             className="m-3 lg:m-5 shadow appearance-none border border-gray-300 rounded h-full py-3 lg:py-4 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             placeholder="Precio"
           />
-          
-            <input
-              type="url"
-              className="m-3 lg:m-5 shadow appearance-none border border-gray-300 rounded h-full py-3 lg:py-4 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              placeholder="Número de teléfono (URL Whatsapp)"
-            />
-  
-            <input
-              type="text"
-              className="m-3 lg:m-5 shadow appearance-none border border-gray-300 rounded h-full py-3 lg:py-4 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              placeholder="Estado del producto"
-            />
-  
-            <button
-              type="submit"
-              className="m-3 lg:m-5 shadow appearance-none border border-gray-300 rounded inline-block py-3 lg:py-4 px-3 text-gray-100 leading-tight focus:outline-none focus:shadow-outline bg-[#2F4D55]"
-            >
-              Publicar anuncio
-            </button>
-          </div>
-        </form>
-      </div>
-    );
-  }
+
+          <input
+            type="url"
+            name="contact"
+            value={productData.contact}
+            onChange={handleChange}
+            className="m-3 lg:m-5 shadow appearance-none border border-gray-300 rounded h-full py-3 lg:py-4 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            placeholder="Número de teléfono (URL Whatsapp)"
+          />
+
+          <input
+            type="text"
+            name="productState"
+            value={productData.productState}
+            onChange={handleChange}
+            className="m-3 lg:m-5 shadow appearance-none border border-gray-300 rounded h-full py-3 lg:py-4 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            placeholder="Estado del producto"
+          />
+
+          <button
+            type="submit"
+            className="m-3 lg:m-5 shadow appearance-none border border-gray-300 rounded inline-block py-3 lg:py-4 px-3 text-gray-100 leading-tight focus:outline-none focus:shadow-outline bg-[#2F4D55]"
+          >
+            Publicar anuncio
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
 
 export default CreateAddMarket;
